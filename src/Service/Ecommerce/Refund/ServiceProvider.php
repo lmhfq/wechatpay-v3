0ea872bc6bedd76e0ae216c64305bdb0bf5df531
lmh <991564110@qq.om>
2026-02-04 17:02:49 +0800

    add

--- src/Service/Ecommerce/Refund/ServiceProvider.php
+++ src/Service/Ecommerce/Refund/ServiceProvider.php
@@ -12,7 +12,7 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $app)
     {
-        $app['refund'] = function ($app) {
+        $app['ecommerceRefund'] = function ($app) {
             return new Client($app);
         };
     }
