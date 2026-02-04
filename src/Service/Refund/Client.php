79164ea8628981d77caee7e5985ecab1c4f017f5
lmh <991564110@qq.om>
2026-02-04 14:34:05 +0800

    增加退款

---
+++ src/Service/Refund/Client.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lmh\WeChatPayV3\Service\Refund;
+
+use Lmh\WeChatPayV3\Kernel\BaseClient;
+
+class Client extends BaseClient
+{
+    public static function className(): string
+    {
+        return 'domestic/refunds';
+    }
+
+    /**
+     * 申请退款
+     * @param array $params
+     * @param array $options
+     * @return array
+     */
+    public function refund(array $params, array $options = []): array
+    {
+        $url = self::classUrl();
+        $opts = $options + ['json' => $params];
+        return $this->request('POST', $url, $opts);
+    }
+
+    /**
+     * @param string $refundId
+     * @param array $params
+     * @param array $options
+     * @return array
+     */
+    public function abnormalRefund(string $refundId, array $params, array $options = []): array
+    {
+        $url = self::instanceUrl($refundId) . '/apply-abnormal-refund';
+        $opts = $options + ['json' => $params];
+        return $this->request('POST', $url, $opts);
+    }
+
+    /**
+     * @param string $outRefundNo
+     * @param $query
+     * @param array $options
+     * @return array
+     */
+    public function query(string $outRefundNo ,$query = null, array $options = []): array
+    {
+        return parent::retrieve($outRefundNo, $query, $options);
+    }
+}
